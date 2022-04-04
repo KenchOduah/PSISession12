28138d745ff36faac64f4931a6f9a69ec0af6959
KenchOduah <79539489+KenchOduah@users.noreply.github.com>
2022-04-04 14:08:40 -0400

    Add files via upload

---
+++ MarkerTester.java
@@ -0,0 +1,16 @@
+/*Create and print 10 objects of the markers class]
+ *(If finished move on below)
+ * 
+ *make an object based off of a users input
+ *
+ *give the new objects variables using .getSetterName
+ * 
+ */
+public class MarkerTester {
+
+	public static void main(String[] args) {
+		//Hint: Marker m1 = something something;
+
+	}
+
+}
---
+++ Markers.java
@@ -0,0 +1,14 @@
+/*Make a class of markers and include the type of marker(String)
+ * color of the marker(String)
+ * and number of markers(int)
+ * Then create the constructors and getters/setters
+ */
+public class Markers {
+
+	
+	public String toString() {
+		return String.format("The type of marker is " + markerType + ".\n The color of the marker is " + markerColor + ".\n"
+				+ "The number of markers is " + numOfMarkers + ".");
+	}
+}
+
