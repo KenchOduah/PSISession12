29ad0bd7f78ed698101e2ea9014dfa1f0e63d2d3
KenchOduah <79539489+KenchOduah@users.noreply.github.com>
2022-04-11 15:36:17 -0400

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,6 +1,6 @@
 /*
 Write a program that would print the information 
-(name, year of joining, salary, address)
+(name, year of joining, salary, address, still hired(boolean))
 of three employees.
  */
 public class Employee {
