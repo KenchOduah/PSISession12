b44808ea589bc3e68dcf64145afdf9d7d50cc18a
KenchOduah <79539489+KenchOduah@users.noreply.github.com>
2022-04-11 15:35:12 -0400

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,8 @@
+/*
+Write a program that would print the information 
+(name, year of joining, salary, address)
+of three employees.
+ */
+public class Employee {
+
+}
---
+++ EmployeeTester.java
@@ -0,0 +1,8 @@
+/*
+ * Create some objects of Employee here and print them
+ * After printing user the setter to alter some of the employees data and print again
+ * `
+ */
+public class EmployeeTester {
+
+}
